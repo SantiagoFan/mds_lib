4b933d897c41281ae79b598172bac12448418e13
SantiagoFan_范文刚 <464884785@qq.com>
2020-03-11 11:39:46 +0800

    Update HttpHelper.php

--- utils/HttpHelper.php
+++ utils/HttpHelper.php
@@ -79,29 +79,50 @@ class HttpHelper
         return json_decode($result,true);
     }
 
-    public static function http_curl($url,$type='get',$res='json',$arr=''){
-        //1.初始化curl
-        $ch = curl_init();
-        //2.设置curl的参数
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        if($type == 'post'){
-            curl_setopt($ch, CURLOPT_POST,1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $arr);
+    /**
+     * @param $url  访问的URL
+     * @param string $post post数据(不填则为GET)
+     * @param string $cookie 提交的$cookies
+     * @param int $returnCookie 是否返回$cookies
+     * @return bool|string
+     */
+     public  function curl_request($url,$post='',$cookie='', $returnCookie=0){
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)');
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($curl, CURLOPT_AUTOREFERER, 1);
+        curl_setopt($curl, CURLOPT_REFERER, "http://XXX");
+        if($post) {
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($post));
         }
-        //3.采集
-        $output = curl_exec($ch);
-        //4.关闭
-        curl_close($ch);
-        if($res=='json'){
-            if(curl_error($ch)){
-                //请求失败，返回错误信息
-                return curl_error($ch);
-            }else{
-                //请求成功，返回信息
-                return json_decode($output,true);
+        if($cookie) {
+            curl_setopt($curl, CURLOPT_COOKIE, $cookie);
+        }
+        curl_setopt($curl, CURLOPT_HEADER, $returnCookie);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $data = curl_exec($curl);
+        if (curl_errno($curl)) {
+            return curl_error($curl);
+        }
+        curl_close($curl);
+        if($returnCookie){
+            list($header, $body) = explode("\r\n\r\n", $data, 2);
+            preg_match_all("/Set\-Cookie:([^;]*);/", $header, $matches);
+
+            $info['cookie']  = [];
+            foreach ($matches[1] as $k=>$v){
+                $ls = explode("=",$v);
+                $info['cookie'][trim($ls[0])] =$ls[1];
             }
+            $info['content'] = json_decode($body,true);
+            return $info;
+        }else{
+            return $data;
         }
     }
+
 
 }

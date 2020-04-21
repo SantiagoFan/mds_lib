a05e72cfb2d2ed522b6e720dd7e7ba9e2b3f38c0
SantiagoFan_范文刚 <464884785@qq.com>
2020-04-21 15:42:36 +0800

    Create Time.php

---
+++ utils/Time.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * 文件名简单介绍
+ *
+ * Author        范文刚
+ * Email         464884785@qq.com
+ * Date          2020/3/31
+ * Time          上午10:51
+ * Version       1.0 版本号
+ */
+
+namespace join\utils;
+
+
+class Time
+{
+    /**
+     * 当前时间
+     * @param string $format
+     * @return false|string
+     */
+    public static function now($format='Y-m-d H:i:s'){
+        return date($format);
+    }
+}
